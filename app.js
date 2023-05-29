fce68fe39304175de81149790a4bafdf0c96780a
sultonbayevogabek <sultonbayevogabek@gmail.com>
2023-05-29 19:45:20 +0500

    changes

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ import { join } from 'path';
 import { readdir } from 'fs';
 import express from 'express';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import { PORT } from './config.js';
+
 const db = require('./modules/db.js')
-const { user } = require('./middlewares/auth.middleware')
+const {user} = require('./middlewares/auth.middleware')
 const app = express()
 
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 app.use(cors())
-app.use(cookieParser())
 app.use(async (req, res, next) => {
    req.db = await db()
    next()
@@ -21,9 +21,12 @@ app.use(user)
 
 readdir(join(__dirname, 'routes'), (err, files) => {
    files.forEach(file => {
-      const router = require(join(__dirname, 'routes', file))
+      const router = import(join(__dirname, 'routes', file))
       app.use(router.route, router.router)
    })
 })
 
-module.exports = { app, server }
+app.listen(PORT, () => {
+   console.log('SERVER RUNNING AT: http://localhost' + PORT)
+})
+
--- server.js
+++
@@ -1,6 +0,0 @@
-const config = require('./config')
-const { app, server } = require('./app')
-
-server.listen(config.PORT, () => {
-    console.log(`Listening on port ${config.PORT}`)
-})
